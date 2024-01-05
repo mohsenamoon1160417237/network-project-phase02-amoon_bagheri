bfaeb9197183bf0df577f1d9cfd4cbd2399016b5
mohsenamoon <dramatic225@gmail.com>
2024-01-05 12:38:04 +0330

    .

--- client.py
+++ client.py
@@ -1,22 +1,37 @@
 import socket
 import os
-import time
 
 HOST = "localhost"  # The server's hostname or IP address
-PORT = 8090  # The port used by the server
+PORT = 8083  # The port used by the server
 hand_shake = False
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
 
 while 1:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
     user_input = input("Enter input: ")
     if user_input[:4] == "STOR":
         if not os.path.isfile(user_input.split(" ")[1]):
             print("invalid file path")
             continue
+    if user_input[:4] == "QUIT":
+        s.sendall(user_input.encode())
+        response = s.recv(4096).decode()
+        print(response)
+        s.close()
+        break
+    if user_input[:4] == "DELE":
+        s.sendall(user_input.encode())
+        confirmation_request = s.recv(4096).decode()
+        print(confirmation_request)
+        if confirmation_request == "File not found":
+            continue
+        confirmation = input()
+        s.sendall(confirmation.encode())
+        delete_response = s.recv(4096).decode()
+        print(delete_response)
+        continue
     s.sendall(user_input.encode())
-    data = s.recv(4096).decode()
-    print(data)
+    data = s.recv(8192).decode()
     if data == "OK1" and not hand_shake:
         print("entered ok1")
         hand_shake = True
@@ -24,8 +39,7 @@ while 1:
         f = fo.read()
         fo.close()
         s.sendall(f)
-        s.send("End")
-        data = s.recv(4096).decode()
+        data = s.recv(8192).decode()
     if data == "OK2" and hand_shake:
         hand_shake = False
     if user_input[:4] == "RETR":
@@ -34,6 +48,12 @@ while 1:
             fw = open(os.path.join(os.getcwd(), file_name), "w")
             fw.write(data)
             fw.close()
+    if user_input[:4] == "QUIT":
+        s.sendall(user_input.encode())
+        response = s.recv(4096).decode()
+        print(response)
+        s.close()
+        break
+
     else:
         print(data)
-    s.close()
--- ftp.py
+++ ftp.py
@@ -1,35 +1,84 @@
 import datetime
 import os
+import shutil
 import socket
-import time
+import threading
+
+users = [
+    {"username": "user1", "password": "123456"}
+]
+
+hand_shakes = {}
+
+authentication = {}
+
+store_paths = {}
+
+
+def authentication_required(function):
+    def wrapper(*args, **kwargs):
+        if authentication.get(args[3]):  # self.authenticated
+            if authentication.get(args[3]).get('authenticated'):
+                function(*args, **kwargs)
+        else:
+            args[2].sendall("Authentication not provided".encode())  # client_socket
+
+    return wrapper
+
+
+class ThreadManager:
+    def __init__(self):
+        self.thread_ids = list()
+        self.counter = 0
+
+    def create_new_thread(self, ftp, c_socket: socket):
+        thread_id = self.counter + 1
+        self.counter += 1
+        th = threading.Thread(target=ftp.main, args=(c_socket, thread_id))
+        self.thread_ids.append(thread_id)
+        th.start()
 
 
 class FTPHandler:
 
-    def __init__(self, serv_socket: socket):
+    def __init__(self, serv_socket: socket, thread_manager: ThreadManager):
         self.server_socket = serv_socket
-        self.hand_shake = False
-        self.store_path = ''
+        self.thread_manager = thread_manager
         host = 'localhost'
-        port = 8090
+        port = 8083
         server_socket.bind((host, port))
         server_socket.listen(1)
         print(f"Server is listening on http://{host}:{port}")
 
     @classmethod
-    def get_user(cls, user: str):
-        pass
+    def get_user(cls, user: str, client_socket: socket, thread_id: int):
+        for u in users:
+            if u.get('username') == user:
+                authentication[thread_id] = {'username': user}
+                client_socket.sendall("Retrieved username".encode())
+                return
+        client_socket.send("User name not found".encode())
 
     @classmethod
-    def get_password(cls, password: str):
-        pass
+    def get_password(cls, password: str, client_socket: socket, thread_id: int):
+        if authentication.get(thread_id):
+            username = authentication[thread_id].get('username')
+            for u in users:
+                if u.get('username', '') == username and password == u.get('password', ''):
+                    authentication[thread_id]['authenticated'] = True
+                    client_socket.sendall("Authentication provided".encode())
+                    return
 
-    @classmethod
-    def show_list(cls, path_name: str, client_socket: socket) -> None:
+            client_socket.send("Wrong password".encode())
+        else:
+            client_socket.send("First provide username".encode())
+
+    @authentication_required
+    def show_list(self, path_name: str, client_socket: socket, thread_id: int) -> None:
         base_path = os.getcwd() if path_name is None else path_name
         is_file = os.path.isfile(base_path)
         if is_file:
-            client_socket.sendall(cls.show_file_context(base_path))
+            client_socket.sendall(self.show_file_context(base_path))
             return
         ls = os.listdir(base_path)
         files_list = list()
@@ -50,8 +99,8 @@ class FTPHandler:
         fo.close()
         return f
 
-    @classmethod
-    def send_file(cls, file_path: str, client_socket: socket) -> None:
+    @authentication_required
+    def send_file(self, file_path: str, client_socket: socket, thread_id: int) -> None:
         if os.path.isfile(file_path):
             fo = open(file_path, 'rb')
             f = fo.read()
@@ -60,44 +109,116 @@ class FTPHandler:
         else:
             client_socket.sendall('Invalid file path'.encode())
 
-    def receive_file(self, file: bytes, client_socket: socket):
-        fw = open(self.store_path, "wb")
+    @authentication_required
+    def receive_file(self, file: bytes, client_socket: socket, thread_id: int):
+        fw = open(store_paths[thread_id], "wb")
         fw.write(file)
         fw.close()
-        self.hand_shake = False
-        self.store_path = ''
+        hand_shakes[thread_id] = False
+        store_paths[thread_id] = ''
         client_socket.send("OK2".encode())
 
-    def main(self):
+    @authentication_required
+    def delete_file(self, file_path: str, client_socket: socket, thread_id: int):
+        if os.path.isfile(file_path):
+            client_socket.send("Do you really wish to delete (Y/N)? ".encode())
+            confirmation = client_socket.recv(1024).decode().strip()
+            if confirmation.lower() == 'y':
+                os.remove(file_path)
+                client_socket.send("File deleted successfully".encode())
+            else:
+                client_socket.send("File deletion canceled".encode())
+        else:
+            client_socket.send("File not found".encode())
+
+    @authentication_required
+    def make_directory(self, dir_path: str, client_socket: socket, thread_id: int):
+        try:
+            if not os.path.exists(dir_path):
+                os.makedirs(dir_path)
+                client_socket.send(f"Directory created: {dir_path}".encode())
+            else:
+                client_socket.send("Directory already exists".encode())
+        except Exception as e:
+            client_socket.send(f"Error creating directory: {e}".encode())
+
+    @authentication_required
+    def remove_directory(self, dir_path: str, client_socket: socket, thread_id: int):
+        if os.path.exists(dir_path) and os.path.isdir(dir_path):
+            shutil.rmtree(dir_path, ignore_errors=True)
+            client_socket.send(f"Directory removed: {dir_path}".encode())
+        else:
+            client_socket.send("Directory not found or not a directory".encode())
+
+    @authentication_required
+    def print_working_directory(self, dummy, client_socket: socket, thread_id: int):
+        pwd = os.getcwd()
+        client_socket.send(f"Current Directory: {pwd}".encode())
+
+    @authentication_required
+    def change_working_directory(self, dir_path: str, client_socket: socket, thread_id: int):
+        if os.path.exists(dir_path) and os.path.isdir(dir_path):
+            os.chdir(dir_path)
+            client_socket.send(f"Changed directory to {dir_path}".encode())
+        else:
+            client_socket.send("Directory not found or not a directory".encode())
+
+    @authentication_required
+    def change_to_parent_directory(self, dummy, client_socket: socket, thread_id: int):
+        os.chdir('..')
+        pwd = os.getcwd()
+        client_socket.send(f"Changed to parent directory: {pwd}".encode())
+
+    def start(self):
         while True:
-            if not self.hand_shake:
-                client_socket, client_address = self.server_socket.accept()
+            client_socket, client_address = self.server_socket.accept()
+            self.thread_manager.create_new_thread(self, client_socket)
+
+    def main(self, client_socket: socket, thread_id: int):
+        while True:
             request = client_socket.recv(1048576)
-            if self.hand_shake:
-                self.receive_file(request, client_socket)
+            if hand_shakes.get(thread_id):
+                self.receive_file(request, client_socket, thread_id)
             else:
                 request = request.decode()
             if request[:4] == "USER":
-                self.get_user(request.split(" ")[1].strip())
+                self.get_user(request.split(" ")[1].strip(), client_socket, thread_id)
             elif request[:4] == "PASS":
-                self.get_password(request.split(" ")[1].strip())
+                self.get_password(request.split(" ")[1].strip(), client_socket, thread_id)
             elif request[:4] == "LIST":
                 path_name = request.split(" ")[1].strip() if len(request.split(" ")) > 1 else None
-                self.show_list(path_name, client_socket)
+                self.show_list(path_name, client_socket, thread_id)
             elif request[:4] == "RETR":
-                self.send_file(request.split(" ")[1].strip(), client_socket)
+                self.send_file(request.split(" ")[1].strip(), client_socket, thread_id)
             elif request[:4] == "STOR":
-                if not self.hand_shake:
-                    self.store_path = request.split(" ")[2].strip()
+                if not hand_shakes.get(thread_id):
+                    store_paths[thread_id] = request.split(" ")[2].strip()
                     client_socket.send("OK1".encode())
-                    self.hand_shake = True
-                # else:
-                #     self.receive_file(request.split("EOF")[0].replace("STOR", "").strip(), client_socket)
-            if not self.hand_shake:
+                    hand_shakes[thread_id] = True
+            elif request[:4] == "DELE":
+                file_path = request.split(" ")[1].strip()
+                self.delete_file(file_path, client_socket, thread_id)
+            elif request[:3] == "MKD":
+                dir_path = request.split(" ")[1].strip()
+                self.make_directory(dir_path, client_socket, thread_id)
+            elif request[:3] == "RMD":
+                dir_path = request.split(" ")[1].strip()
+                self.remove_directory(dir_path, client_socket, thread_id)
+            elif request[:4] == "PWD":
+                self.print_working_directory(None, client_socket, thread_id)
+            elif request[:3] == "CWD":
+                dir_path = request.split(" ")[1].strip()
+                self.change_working_directory(dir_path, client_socket, thread_id)
+            elif request[:4] == "CDUP":
+                self.change_to_parent_directory(None, client_socket, thread_id)
+            elif request[:4] == "QUIT":
+                client_socket.send("Connection closed".encode())
                 client_socket.close()
+                break
 
 
 if __name__ == '__main__':
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ftp_handler = FTPHandler(server_socket)
-    ftp_handler.main()
+    thread_man = ThreadManager()
+    ftp_handler = FTPHandler(server_socket, thread_man)
+    ftp_handler.start()
